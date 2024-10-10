f37f78053b8f48efdc82e2e50e7387fd49f0df88
Sreehari Sreedev <me@ssree.dev>
2024-10-09 19:15:39 -0700

    InstructorLogin page

--- CSE360_Project/src/InstructorLoginController.java
+++ CSE360_Project/src/InstructorLoginController.java
@@ -1,4 +1,27 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class InstructorLoginController {
-
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	private Storage storage;
+	
+	@FXML
+	private Text ip_userLabel;
+	
+	@FXML
+	public void ip_logoutClicked(ActionEvent event) throws IOException{
+		
+	}
+	
+	public void userName(String name) {
+		this.ip_userLabel.setText("User: " + name);
+	}
 }
