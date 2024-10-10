5b291f341cb731d4e46f40c5b93c2f5ee3f8d02a
Sreehari Sreedev <me@ssree.dev>
2024-10-09 19:17:40 -0700

    StudentLoginController

--- CSE360_Project/src/StudentLoginController.java
+++ CSE360_Project/src/StudentLoginController.java
@@ -1,4 +1,27 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class StudentLoginController {
-
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	private Storage storage;
+	
+	@FXML
+	private Text sl_userLabel;
+	
+	@FXML
+	public void sl_logoutClicked(ActionEvent event) throws IOException{
+		
+	}
+	
+	public void userName(String name) {
+		this.sl_userLabel.setText("User: " + name);
+	}
 }
