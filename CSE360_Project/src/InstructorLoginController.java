0df41bcaaad65f5cc241e83586c33a45883d74b5
Sreehari Sreedev <me@ssree.dev>
2024-10-09 19:39:27 -0700

    Instructor and Student logout buttons work

--- CSE360_Project/src/InstructorLoginController.java
+++ CSE360_Project/src/InstructorLoginController.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
@@ -18,7 +20,13 @@ public class InstructorLoginController {
 	
 	@FXML
 	public void ip_logoutClicked(ActionEvent event) throws IOException{
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScene.fxml"));
+		root = loader.load();
 		
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 	}
 	
 	public void userName(String name) {
