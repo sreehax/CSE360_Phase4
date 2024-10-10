49ffa6108a295237cf21c3d940910794db0fdae0
Allan <90856446+allanbinu197@users.noreply.github.com>
2024-10-09 17:52:10 -0700

    Update CreateAccountController.java
    DOCS

--- CSE360_Project/src/CreateAccountController.java
+++ CSE360_Project/src/CreateAccountController.java
@@ -12,7 +12,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-
+/**
+ * The CreateAccountController class manages the account creation process
+ * for users within the application. It handles user input, validates
+ * passwords, and interacts with the Storage class to register new users.
+ */
 public class CreateAccountController {
 	private Stage stage;
 	private Scene scene;
@@ -25,7 +29,14 @@ public class CreateAccountController {
 	private TextField ca_usernameid, ca_passwordid, ca_password2id;
 	@FXML
 	private Text ca_nouserstxt;
-	
+	/**
+     * Handles the event when the create account button is clicked.
+     * Validates user input, checks password matching, and registers
+     * the new user in the storage.
+     *
+     * @param event The action event triggered by the button click
+     * @throws IOException If there is an issue loading the next scene
+     */
 	@FXML
 	public void create_account_buttonClicked(ActionEvent event) throws IOException {
 		
@@ -56,7 +67,7 @@ public class CreateAccountController {
 		try {
 			String listofRoles = s.getRolesFromCode(currentCode);
 			System.out.println(listofRoles);
-			
+			// Check which roles are assigned based on the invite code
 			for (int i = 0; i < listofRoles.length(); i++) {
 				c = listofRoles.charAt(i);
 				c = Character.toLowerCase(c);
@@ -74,11 +85,11 @@ public class CreateAccountController {
 			}
 			
 			//interpret list of roles.
-			
+			// Create new user and set properties
 			User user = new User();
 			user.setUsername(username);
 			user.setCode(currentCode);
-			
+			// Add roles to user based on flags
 			if(admincreation) {
 				user.addRole('a');
 			}
@@ -92,7 +103,7 @@ public class CreateAccountController {
 				user.addRole('s');
 			}
 			
-			
+			// Register the new user and login information
 			s.registerLogin(username, password);
 			s.registerUser(user);
 			System.out.println("Successfully registered user!");
@@ -104,34 +115,45 @@ public class CreateAccountController {
 		}
 		
 		this.admincreation = false;
-		/*
+	}
+	/*
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScene.fxml"));
 		root = loader.load();
 		
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
-        stage.show(); */
-	}
-	
+        stage.show();
+	@param event The action event triggered by the button click
+ 	@throws IOException If there is an issue loading the previous scene
+     */
 	@FXML
 	public void create_account_backClicked(ActionEvent event) throws IOException {
 		if (this.admincreation) {
 			ca_nouserstxt.setText("You have to register an admin first");
 			return;
 		}
+		// Load the main scene if an admin has been created
 		root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
 	}
-	
+	 /**
+     * Clears the message displayed to the user regarding
+     * the necessity of creating an admin user.
+     */
 	public void deletenousermsg() {
 		ca_nouserstxt.setText("");
 		this.admincreation = false;
 	}
-	
+	/**
+     * Initializes the controller with a one-time invite code
+     * for user registration and sets the corresponding message.
+     *
+     * @param code The one-time invite code provided for registration
+     */
 	public void onetimeinvite(String code) {
 		ca_nouserstxt.setText("One Time Invite Code User Registration");
 		this.admincreation = false;
