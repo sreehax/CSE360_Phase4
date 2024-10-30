87224d11c4de6bfe2889fc14b037f3ff289c28a1
programmer-diddy <theonetruefelix@proton.me>
2024-10-30 10:12:45 -0700

    Updated controller class

--- CSE360_Project/src/CreateArticleController.java
+++ CSE360_Project/src/CreateArticleController.java
@@ -1,17 +1,23 @@
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.sql.SQLException;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
 
 //
 public class CreateArticleController {
@@ -19,10 +25,19 @@ public class CreateArticleController {
 	private Scene scene;
 	private Parent root;
 	
-	private TextField titleTextField, referenceLinksTextField, headersTextField, groupsTextField, descriptionTextField, bodyTextField
+	private TextField titleTextField, referenceLinksTextField, headersTextField, groupsTextField, descriptionTextField, bodyTextField;
+	private Button createArticleButton;
 	
-	public void s(ActionEvent event) throws IOException {
+	String titleString, referenceString, headersString, groupsString, descriptionString, bodyString;
+	
+	public void pressCreateArticleButton(ActionEvent event) throws IOException {
+		titleString = titleTextField.getText();
+		referenceString = referenceLinksTextField.getText();
+		headersString = headersTextField.getText();
+		groupsString = groupsTextField.getText();
+		descriptionString = descriptionTextField.getText();
+		bodyString = bodyTextField.getText();
 		
 	}
-
+	
 }
