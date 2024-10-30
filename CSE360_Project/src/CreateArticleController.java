290977498692fc91aac112d25ce10aea282670be
programmer-diddy <theonetruefelix@proton.me>
2024-10-30 09:22:04 -0700

    Added IDs to the buttons

---
+++ CSE360_Project/src/CreateArticleController.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import
+
+//
+public class CreateArticleController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	private TextField titleTextField, referenceLinksTextField, headersTextField, groupsTextField, descriptionTextField, bodyTextField
+	
+	public void s(ActionEvent event) throws IOException {
+		
+	}
+
+}
