f1b56b23ca873a3ecd067b7eae242a6d07a6b9ae
Allan <90856446+allanbinu197@users.noreply.github.com>
2024-10-30 22:38:56 -0700

    Update EditArticleController.java
    DOCS

--- CSE360_Project/src/EditArticleController.java
+++ CSE360_Project/src/EditArticleController.java
@@ -24,7 +24,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//
+/**
+ * The EditArticleController class is responsible for handling the editing of existing articles.
+ * It interacts with various UI components for article attributes such as title, references, 
+ * headers, groups, description, keywords, and body, allowing the user to modify these fields 
+ * and update the article in the database.
+ */
 public class EditArticleController {
 	private Stage stage;
 	private Scene scene;
@@ -45,13 +50,22 @@ public class EditArticleController {
 	private Text e_userLabel, e_idNumLabel;
 	
 	String titleString, referenceString, headersString, groupsString, descriptionString, bodyString, keywordString;
-	
+	  /**
+     * Handles the action triggered when the "Edit Article" button is clicked.
+     * Gathers updated user input from the UI fields, creates a modified Article object 
+     * with the new data, and updates the article in the database using the Storage class.
+     * After updating, it navigates back to the ManageArticles.fxml view.
+     *
+     * @param event The ActionEvent triggered by clicking the "Edit Article" button
+     * @throws IOException if there is an issue loading the ManageArticles.fxml file
+     * @throws SQLException if there is an error during the database operation
+     */
 	@FXML
 	public void e_editArticleClicked(ActionEvent event) throws IOException, SQLException {
-		
+		 // Obtain the current stage and retrieve the Storage object
 		this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         this.storage = (Storage) stage.getUserData();
-        
+        // Gather updated article details from the input fields
 		titleString = e_titleTextField.getText();
 		referenceString = e_referenceLinksTextField.getText();
 		headersString = e_headersTextField.getText();
@@ -63,7 +77,7 @@ public class EditArticleController {
 		//NOTE: Keywords are going to be separated by spaces
 	    String[] referenceArray = referenceString.split(" ");
 	    String[] keywordsArray = keywordString.split(" ");
-	        
+	        // Create a new article and populate its fields with updated values
 		Article newArticle = new Article();
 		newArticle.setTitle(titleString);
 		for(int i = 0; i < referenceArray.length; i++) {
@@ -102,12 +116,23 @@ public class EditArticleController {
         stage.setScene(scene);
         stage.show();
 	}
-	
+	 /**
+     * Sets the username label to display the currently logged-in user's name.
+     *
+     * @param username The username to be displayed
+     */
 	public void userName(String username) {
 		e_userLabel.setText("User: " + username);
 		myusername = username;
 	}
 	
+    /**
+     * Sets the fields in the edit article form with the current data of an article.
+     * Populates the fields such as title, references, header, groups, keywords, 
+     * description, and body, and displays the article's ID.
+     *
+     * @param a The Article object whose details are to be edited
+     */
 	public void setArticle(Article a) {
 		myid = a.getID();
 		e_idNumLabel.setText("ID: " + myid);
@@ -119,7 +144,12 @@ public class EditArticleController {
 		e_descriptionTextField.setText(a.getDescription());
 		e_bodyTextField.setText(a.getBody());
 	}
-	
+	  /**
+     * Sets the origin location indicating where the user navigated from.
+     * This information can be used to customize behavior based on the previous page.
+     *
+     * @param loc The name of the location or page the user came from
+     */
 	public void cameFrom(String loc) {
 		this.camefrom = loc;
 	}
