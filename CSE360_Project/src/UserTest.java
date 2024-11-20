e02e22986fef4cd7d73f48da71f03c0746d377b8
ben-nelson24 <bdnelso6@asu.edu>
2024-11-20 08:07:15 -0700

    Added first JUnit test file

---
+++ CSE360_Project/src/UserTest.java
@@ -0,0 +1,113 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+	@Test
+	void testUser() {
+		User u = new User();
+	}
+
+	@Test
+	void testUserStringStringStringStringStringStringStringInt() {
+		User u = new User("Benjame","D", "Nelson", "Ben", "123456", "2345","34", 123);
+	}
+
+	@Test
+	void testSetFirstname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMiddlename() {
+		User u = new User();
+		u.setPreferredname("Joe");
+	}
+
+	@Test
+	void testSetLastname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPreferredname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTemppass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFirstname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMiddlename() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLastname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPreferredname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTemppass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddRole() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteRole() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetRoleList() {
+		fail("Not yet implemented");
+	}
+
+}
