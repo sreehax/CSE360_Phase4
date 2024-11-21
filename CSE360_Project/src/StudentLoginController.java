258de79049c9a0c375aace4bbf7bd71ea4070c5b
programmer-diddy <theonetruefelix@proton.me>
2024-11-20 17:36:53 -0700

    The listing functionality of the student class works. For the time being, articles should print to the console but that requires database functionality.

--- CSE360_Project/src/StudentLoginController.java
+++ CSE360_Project/src/StudentLoginController.java
@@ -14,6 +14,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DialogPane;
@@ -21,6 +22,12 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+
+//PLEASE READ:
+//
+//To add an article to the list: addArticleToList(<Article Name Here>);
+//To display an article after being clicked, print showSelectedArticle(); to the command line
+
 
 //AUTHOR: Felix Allison & Someone else, probably Jonathan Lin since
 //A lot of the code looks kinda bad.
@@ -42,106 +49,127 @@ public class StudentLoginController{
 	//This should explain the difference in naming schema.
 	
 	@FXML
-	private ComboBox<?> FilterComplexityComboBox, FilterByGroupComboBox;
-	private TextField SearchByKeywordTextField, SearchByArticleIDTextField;
-	private TextArea SubmitHelpMessageTextArea;
-	private Button SearchByKeywordButton, SearchByArticleIDButton, SubmitGeneralHelpMessageButton;
-	private Button SubmitSpecificHelpMessageButton;
-	private Button ResetSearchButton, OpenArticleButton;
-	
+	private Button SBK, SBAID, RS, SGHM, SSHM;
 	@FXML
-	private ListView<Button> ArticleListListView = new ListView<Button>();
+	private ComboBox FBG, FC;
 	@FXML
-	private ObservableList<Button> articleButtons = FXCollections.observableArrayList();
+	private TextField SBKText, SBAIDText;
+	@FXML
+	private TextArea SHM;
+	@FXML
+	private ListView<String> ALLV= new ListView();
+	@FXML
+	private ObservableList<String> listItems = FXCollections.observableArrayList();
 	@FXML
 	private Text sl_userLabel;
+	@FXML
+	private Text AText;
 	
-	//
-	//SREE: Implement database functionality
-	public void filterComplexityComboBoxClicked(ActionEvent event) throws IOException{
-		//Not quite sure what this does
-		//Sree: Implement database functionality with this method.
-	}
-	//SREE: Implement database functionality
-	public void filterByGroupComboBoxClicked(ActionEvent event) throws IOException{
-		//Filters by article group
-		//Sree: Implement database functionality with this method.
-	}
-	//SREE: Implement database functionality
-	public void searchByKeywordButtonPressed(ActionEvent event) throws IOException{
-		Article newArticle = new Article();
-		newArticle.setBody("This is the article body for testing.");
-		newArticle.setDescription("This is a description that says stuff.");
-		newArticle.setGrouping("This is the grouping area. It does something.");
-		newArticle.setHeader("Insert header here");
-		newArticle.setID(12345);
-		newArticle.setTitle("Generic Article Title");
-		newArticle.addKeyword("Keyword1");
-		newArticle.addReference("Reference1");
-	}
-	//SREE: Implement database functionality
-	public void searchByArticleIDButtonPressed(ActionEvent event) throws IOException{
-		//Depending on which is easier to search, I've included both the String and
-		//integer variants of the ID value.
-		String IDtoSearchString = SearchByArticleIDTextField.getText();
-		int IDtoSearchInt = Integer.parseInt(IDtoSearchString);
-	}
-	//SREE: Implement database functionality
-	public void submitGeneralHelpMessageButtonPressed(ActionEvent event) throws IOException{
-		//Send to database
-		//Sree: Implement database functionality with this method.
-		String generalHelpMessage = SubmitHelpMessageTextArea.getText();
+	@FXML
+	public void searchByKeywordPressed(ActionEvent event) throws IOException{
+		String keyword = SBKText.getText();
 	}
 	@FXML
-	//SREE: Implement database functionality
-	public void submitSpecificHelpMessageButtonPressed(ActionEvent event) throws IOException{
-		String specificHelpMessage = SubmitHelpMessageTextArea.getText();
+	public void searchByArticleIDPressed(ActionEvent event) throws IOException{
+		String stringID = "";
+		int intID = 0;
+		
+		if(SBAIDText.getText() != "" && SBAIDText.getText() != null) {
+			stringID = SBAIDText.getText();
+		}
 		
 	}
-	
 	@FXML
-	//COMPLETE
+	public void filterByGroupPressed(ActionEvent event) throws IOException{
+		
+	}
+	@FXML
+	public void filterComplexityPressed(ActionEvent event) throws IOException{
+		
+	}
+	@FXML
 	public void resetSearchButtonPressed(ActionEvent event) throws IOException{
-		ArticleListListView.getItems().clear();
+		listItems.clear();
+	}
+	@FXML
+	public void submitGeneralHelpMessagePressed(ActionEvent event) throws IOException{
+		String generalHelpMessage = SHM.getText();
+	}
+	@FXML
+	public void submitSpecificHelpMessagePressed(ActionEvent event) throws IOException{
+		String specialHelpMessage = SHM.getText();
 	}
 	
-	@FXML
-	//SREE: Implement database functionality
-	public void addArticleToArticleListView(Article articleToInsert) {
-		//Construct a bigass String to insert into the list  
-		String insertable = "";
-		insertable += "Title: " +articleToInsert.getTitle() + "\n";
-		insertable += "Body:" + articleToInsert.getBody() + "\n";
-		insertable += "Keywords: " + articleToInsert.getReferencesStr() + "\n";
-		insertable += "ID: " + articleToInsert.getID()+ "\n";
-		insertable += "Header: " + articleToInsert.getHeader() + "\n";
-		insertable += "Grouping: " + articleToInsert.getGrouping() + "\n";
-		insertable += "Description: " + articleToInsert.getDescription() + "\n";
-		insertable += "Keywords: " + articleToInsert.getKeywordsStr() + "\n";
-		
-		Button tempButton = new Button();
-		tempButton.setText(articleToInsert.getTitle());
-		articleButtons.add(tempButton);
-		ArticleListListView.refresh();
-	}
-	
-	@FXML
-	public void openArticleButtonClicked(ActionEvent event) throws IOException{
-		DialogPane openArticle = new DialogPane();
-		openArticle.snapPositionX(0);
-		openArticle.snapPositionY(0);
-		openArticle.resize(200, 200);
-		
-	}
-    
-	@FXML
 	public void initialize() {
-		ArticleListListView.setItems(articleButtons);
+		//testingMethod();
+		ALLV.setItems(listItems);
+		ALLV.refresh();
 	}
 	
-	//
+	public void addArticleToList(Article articleToInsert) {
+		listItems.add(articleToInsert.getTitle());
+	}
+	
+	public void showSelectedArticle(Article articleToDisplay) {
+		String insertable = "";
+		insertable += "Title: " + articleToDisplay.getTitle() + "\n";
+		insertable += "Body: " + articleToDisplay.getBody() + "\n";
+		insertable += "Keywords: " + articleToDisplay.getReferencesStr() + "\n";
+		insertable += "ID: " + articleToDisplay.getID()+ "\n";
+		insertable += "Header: " + articleToDisplay.getHeader() + "\n";
+		insertable += "Grouping: " + articleToDisplay.getGrouping() + "\n";
+		insertable += "Description: " + articleToDisplay.getDescription() + "\n";
+		insertable += "Keywords: " + articleToDisplay.getKeywordsStr() + "\n";
+		
+		//WRITE TO COMMAND LINE?
+	}
+
+	
+	public void testingMethod() {
+		Article firstArticle = new Article();
+		firstArticle.setBody("Body");
+		firstArticle.setDescription("Description");
+		firstArticle.setGrouping("Group");
+		firstArticle.setHeader("Header");
+		firstArticle.setID(123);
+		firstArticle.setTitle("Title");
+		
+		Article secondArticle = new Article();
+		secondArticle.setBody("Body");
+		secondArticle.setDescription("Description");
+		secondArticle.setGrouping("Group");
+		secondArticle.setHeader("Header");
+		secondArticle.setID(124);
+		secondArticle.setTitle("Title");
+		
+		Article thirdArticle = new Article();
+		thirdArticle.setBody("Body");
+		thirdArticle.setDescription("Description");
+		thirdArticle.setGrouping("Group");
+		thirdArticle.setHeader("Header");
+		thirdArticle.setID(124);
+		thirdArticle.setTitle("Title");
+	
+		Article fourthArticle = new Article();
+		fourthArticle.setBody("Body");
+		fourthArticle.setDescription("Description");
+		fourthArticle.setGrouping("Group");
+		fourthArticle.setHeader("Header");
+		fourthArticle.setID(124);
+		fourthArticle.setTitle("Title");
+		
+		addArticleToList(firstArticle);
+		addArticleToList(secondArticle);
+		addArticleToList(thirdArticle);
+		addArticleToList(fourthArticle);
+	}
+	
+	
+	
+	
+	
+	
 	@FXML
-	//public void logoutButtonPressed(ActionEvent event) throws IOException
 	public void sl_logoutClicked(ActionEvent event) throws IOException{
 		// Load the MainScene.fxml file and set it as the root of the scene
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScene.fxml"));
